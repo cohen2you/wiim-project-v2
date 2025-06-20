7894874dd5e635af08fe8c4f8ffe192acdb8182a
Mike Cohen <cohen2you@gmail.com>
2025-06-20 10:23:47 -0400

    Fix PromptTemplate typing to use inferred input type

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,5 @@
+import { ZodType, z } from 'zod';
+
 export interface StorySection {
   title: string;
   content: string;
@@ -12,9 +14,12 @@ export interface StockData {
   oneYearChange: number;
 }
 
-export interface PromptTemplate<InputSchema = any> {
+// Fix: Make PromptTemplate generic on ZodType and infer input data type for prompt()
+export interface PromptTemplate<
+  InputSchema extends ZodType<any, any, any> = ZodType<any, any, any>
+> {
   name: string;
   inputSchema: InputSchema;
   system: string;
-  prompt: (input: InputSchema) => string;
+  prompt: (input: z.infer<InputSchema>) => string; // Use inferred type here, NOT InputSchema itself
 }
